93e0115652822e052260c35504315a9b7717bbb8
revillnik <revillnik@gmail.com>
2024-01-08 11:27:03 +0300

    next

--- dataclass.py
+++ dataclass.py
@@ -1,16 +1,18 @@
 from dataclasses import dataclass, field
 
-
 @dataclass
 class Data:
     name: str
     price: int
     area: list = field(default_factory=list)
+    def __eq__(self, other):
+        return self.price == other.price
 
 
-a = Data("a", 25000)
+a = Data("a", 14000)
 a.area.append(10)
 b = Data('b', 14000)
 print(a)
 print(b)
+print(a == b)
 
