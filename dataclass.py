77ad47b8ad67c8b5a516236a52c57239b2d122e0
revillnik <revillnik@gmail.com>
2024-01-08 12:12:16 +0300

    next

--- dataclass.py
+++ dataclass.py
@@ -1,18 +1,25 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 
-@dataclass
+
+@dataclass(eq=True, order=True)
 class Data:
-    name: str
+    name: str = field(compare=False)
     price: int
-    area: list = field(default_factory=list)
+    area: list = field(default_factory=list, compare=False)
+    sell: bool = field(default=True)
+    allprice: float = field(init=False)
+
     def __eq__(self, other):
         return self.price == other.price
+
+    def __post_init__(self):
+        if self.sell:
+            self.allprice = self.price * 0.9
+
 
 
 a = Data("a", 14000)
 a.area.append(10)
-b = Data('b', 14000)
+a.sell = False
+print(a.sell)
 print(a)
-print(b)
-print(a == b)
-
